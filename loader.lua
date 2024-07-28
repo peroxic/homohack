83b4a3035df37239c05a141e7b5e36504b649f4f
Peroxic <163497761+peroxic@users.noreply.github.com>
2024-07-28 01:03:19 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,10 +6,10 @@ local GameId = game.GameId
 --// Tables
 
 local Games = {
-    { name = "Rivals", gameid = 6035872082, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/rivals.lua" },
-    { name = "Bad Business", gameid = 1168263273, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/bad%20business.lua" },
-    { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua", rewrite = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua" },
-    { name = "Phantom Forces Test Place", gameid = 115272207, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua" },
+    { name = "Rivals", gameid = 6035872082, link = "https://raw.githubusercontent.com/peroxic/homohack/main/rivals.lua" },
+    { name = "Bad Business", gameid = 1168263273, link = "https://raw.githubusercontent.com/peroxic/homohack/main/bad%20business.lua" },
+    { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/peroxic/homohack/main/placeholder/chams.lua", rewrite = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua" },
+    { name = "Phantom Forces Test Place", gameid = 115272207, link = "https://raw.githubusercontent.com/peroxic/homohack/main/placeholder/chams.lua" },
 }
 
 function Fetch(URL)
@@ -20,7 +20,7 @@ end
 
 for _, Supported in Games do
     if Supported.gameid == GameId then
-        Library:Notify("Homohack detected you being in " .. Supported.name .. ", now loading script...", 5)
+        Library:Notify("ObeseHack detected you being in " .. Supported.name .. ", now loading script...", 5)
 
         if Supported.name:find("Phantom Forces") and getgenv()["load_rewrite"] then
             loadstring(Fetch(Supported.rewrite))()
@@ -32,5 +32,5 @@ for _, Supported in Games do
     end
 end
 
-Library:Notify("You are not in a homohack supported game, loading universal...")
-loadstring(Fetch("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/universal.lua"))()
+Library:Notify("You are not in a ObeseHack supported game, loading universal...")
+loadstring(Fetch("https://raw.githubusercontent.com/peroxic/homohack/main/universal.lua"))()
